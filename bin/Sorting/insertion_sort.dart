3f711e3791c53f7a57556342eaea8d540cca23b8
Arya-Vs <87410819+Arya-Vs@users.noreply.github.com>
2024-03-06 19:37:35 +0530

    insertion sort

---
+++ bin/Sorting/insertion_sort.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  List<int>array=[3,5,8,9,7,8,9,1,2,3,4,5];
+  insertionSort(array: array);
+  print(array);
+}
+
+insertionSort({required List<int>array}){
+  for(int i=0;i<array.length;i++){
+    int temp = array[i];
+    int j = i-1;
+    while(j>=0 && array[j]>temp){
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1] = temp;
+  }
+}
--- bin/Study/study.dart
+++ bin/Study/study.dart
@@ -1,30 +1,18 @@
-import '../Recursion/recursive_binary_search.dart';
-
-void main(){
-  List<int>arr=[1,4,2,5,3,7];
-  arr.sort();
-  int target=5;
-  int result=binarySearch(arr, target);
-  print(arr);
-  print(result);
-  
+class Solution {
+  List<int> numberOfPairs(List<int> nums) {
+Map<int,int>map={};
+for(int num in nums){
+  map[num]=(map[num]??0)+1;
 }
-
-int binarySearch(List<int>arr,int target){
-int leftindex=0;
-int rightindex=arr.length-1;
-
-while (leftindex<=rightindex) {
-  int middileindex=(leftindex+rightindex)~/2;
-  if (arr[middileindex]==target) {
-    return middileindex;
-  }
-  else if(arr[middileindex]<target){
-    leftindex=middileindex+1;
-  }
-  else{
-    rightindex =middileindex-1;
+int paircount=0;
+int leftcount=0;
+for(int count in map.values){
+  paircount +=count~/2;
+  if(count %2!=0){
+    leftcount++;
   }
 }
-return -1;
+return [paircount,leftcount];
+  }
 }
+ 
